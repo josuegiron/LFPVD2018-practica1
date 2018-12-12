82f6f69b6e195bc58d9de019a2a138c1bf0dc1b9
Josué Girón <josue.giron@mnc.com>
2018-12-12 00:42:17 -0600

    Inicio del proyecto

---
+++ TecnoTeamProcess/src/tecnoteamprocess/TecnoTeamProcess.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tecnoteamprocess;
+
+/**
+ *
+ * @author josue
+ */
+public class TecnoTeamProcess {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
