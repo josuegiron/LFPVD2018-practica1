ad9f26af31e97e73b051399fabab2cbc0a43e976
Josué Girón <josue.giron@mnc.com>
2018-12-12 04:09:52 -0600

    Se termina GUI, funciones de abrir, guardar y guardar como.

--- TecnoTeam/src/frontend/datahandler.java
+++ TecnoTeam/src/frontend/datahandler.java
@@ -3,19 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tecnoteamprocess;
+package frontend;
+
+import javax.swing.JFileChooser;
+import javax.swing.JTextArea;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
  * @author josue
  */
-public class TecnoTeamProcess {
+public class datahandler {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
